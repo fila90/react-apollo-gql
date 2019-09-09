b0f473ddc874b049170ed1b85e465af1f3b51200
fila90 <ivan.filipovic.fila@gmail.com>
2019-09-09 12:46:34 +0200

    add dumb styled components

---
+++ src/components/styled/ViewWrap.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const ViewWrap = styled.main`
+  max-width: 1200px;
+  position: relative;
+  font-size: 1.4rem;
+  margin: auto;
+  overflow: auto;
+`;
+
+export default ViewWrap;
