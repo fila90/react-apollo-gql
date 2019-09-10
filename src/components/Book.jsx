7a0fce66fa546372ad21c44aa0abb33b3dd65217
fila90 <ivan.filipovic.fila@gmail.com>
2019-09-10 02:39:37 +0200

    add checkbox to add book to the selected array

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useMutation } from '@apollo/react-hooks';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Edit from '../assets/edit.png';
+import { TOGGLE_BOOK, GET_TOTAL_PRICE, GET_BOOKS } from '../api';
 
 const BookWrap = styled.div`
   position: relative;
@@ -19,11 +21,9 @@ const BookAuthor = styled.h4`
   margin: 0 0 0.5rem 0;
 `;
 const BookPrice = styled.p`
-  position: absolute;
-  bottom: 1rem;
-  right: 1rem;
   margin: 0;
   font-size: 1rem;
+  font-weight: bold;
 `;
 const BookEdit = styled(Link)`
   position: absolute;
@@ -34,12 +34,42 @@ const BookEditImg = styled.img`
   width: 2rem;
 `;
 
+const BookLabel = styled.label`
+  position: absolute;
+  right: 1rem;
+  bottom: 1rem;
+`;
+
 /**
  * @desc single book components
  * display all info regarding the book
  * integrate edit link
  */
 export default function Book({ book }) {
+  const [toggleBook] = useMutation(TOGGLE_BOOK, {
+    variables: { bookId: book.bookId },
+    // update book.isSelected field and recalculate totalPrice
+    update: (cache, { data: { toggleBook } }) => {
+      try {
+        const { books } = cache.readQuery({ query: GET_BOOKS });
+        const newBooks = books.map(b => {
+          if (b.bookId === toggleBook) {
+            b.isSelected = !b.isSelected;
+          }
+          return b;
+        });
+        cache.writeQuery({
+          query: GET_BOOKS,
+          data: { books: newBooks },
+        });
+      } catch (err) {
+        console.error('books not fetched');
+      }
+    },
+    refetchQueries: [{ query: GET_TOTAL_PRICE }],
+    awaitRefetchQueries: true,
+  });
+
   return (
     <BookWrap>
       <b>{book.bookId}</b>
@@ -49,6 +79,16 @@ export default function Book({ book }) {
       <BookEdit to={`/edit/${book.bookId}`}>
         <BookEditImg src={Edit} alt='edit book' title='edit book' />
       </BookEdit>
+
+      <BookLabel>
+        {!book.isSelected ? 'add' : 'remove'}
+        <input
+          type='checkbox'
+          name='selected'
+          checked={book.isSelected}
+          onChange={toggleBook}
+        />
+      </BookLabel>
     </BookWrap>
   );
 }
