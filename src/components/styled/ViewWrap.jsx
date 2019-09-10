08368b67e12f6f3cbac37a75cb81b88d831ae207
fila90 <ivan.filipovic.fila@gmail.com>
2019-09-10 02:39:08 +0200

    update style

--- src/components/styled/ViewWrap.jsx
+++ src/components/styled/ViewWrap.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const ViewWrap = styled.main`
   max-width: 1200px;
   position: relative;
+  padding: 1rem;
   font-size: 1.4rem;
   margin: auto;
   overflow: auto;
